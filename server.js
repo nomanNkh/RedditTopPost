7c711e5122c381b8dcd30d22ecf7dd829229f211
Noman Khan <zceenkh@ucl.ac.uk>
2019-11-20 01:39:18 +0000

    changed port2

--- server.js
+++ server.js
@@ -3,7 +3,7 @@
 //libraries and instances
 const express = require('express')
 const app = express()
-const port = 3000
+const port = process.env.PORT || 5000;
 app.use(express.static(__dirname));
 
 let request = require('request');
