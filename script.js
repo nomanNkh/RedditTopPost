ac9458a5861e15a425c2eba586d49cb65909aed0
Noman Khan <zceenkh@ucl.ac.uk>
2019-11-20 01:28:48 +0000

    make it better

--- script.js
+++ script.js
@@ -1,11 +1,20 @@
-// JavaScript source code
+const searchReddit = document.getElementsByClassName("search-btn")[0];
+searchReddit.addEventListener("click", search);//starts the program when the search button is clicked.
+
 var subreddit = ''
 var name = ''
 var author = ''
 var text = ''
+
 async function search() {
-    subreddit = document.getElementById("subreddit").value;
-    const requ = '/api/getTopPost?subreddit=' + subreddit
+    subreddit = document.getElementById("subreddit").value;//gets searched result from html to this file
+
+    if (subreddit == null|| subreddit == ''|| subreddit == ' ') {
+        subreddit = 'adadazxcz';//fake subreddit when subreddit doesnt exist
+    }
+
+    //calling REST API
+    const requ = '/api/getTopPost/' + subreddit;
     fetch(requ)
         .then(res=>res.json())
         .then(data => {
@@ -17,13 +26,12 @@ async function search() {
 }
 
 async function display(name, author, text) {
+    //display results in HTML file.
     var container = document.getElementsByClassName("results-container")[0];
     if (name == '') {
-        container.innerHTML = '<font color = "white"> <br> This Subreddit Doesn\'t Exist </font> <br>';
+        container.innerHTML = '<font color = "white"> <br> This Subreddit Doesn\'t Exist Try Again </font> <br>';
     } else {
         container.innerHTML = '<font color = "white">' + '<br>' + name + " <br><br> Posted by:     " + author + "<br><br>" + text + '</font> <br>';
     }
 }
-const searchReddit = document.getElementsByClassName("search-btn")[0];
-searchReddit.addEventListener("click", search);
 
--- server.js
+++ server.js
@@ -1,4 +1,6 @@
+//REST API
 
+//libraries and instances
 const express = require('express')
 const app = express()
 const port = 3000
@@ -6,32 +8,37 @@ app.use(express.static(__dirname));
 
 let request = require('request');
 app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/index.html');
+    //res.send('Hello world'); //uncomment this if you want to test rest api itself without html page
+    res.sendFile(__dirname + '/index.html');//redirects to html page
 })
 
-app.get('/api/getTopPost', (req, res) => {
-    //var ans = "";
+app.get('/api/getTopPost/:subreddit', (req, res) => {
     var baseurl = 'https://www.reddit.com/r/';
     var topurl = '/top.json?limit=1';
-    //var subreddit = 'cscareerquestion';
-    var subreddit = req.query.subreddit;
-    var apiUrl = baseurl + subreddit + topurl; 
-
+    var subreddit = req.params.subreddit;//puts subreddit from request into variable to call reddit api
+    var apiUrl = baseurl + subreddit + topurl; //reddit api
     request(apiUrl, function (err, response, body) {
         if (err) {
-            console.log('error:', error);
             res.redirect('/error');
         } else {
+            //extracting relevant data from rest api
+
             var topPost = JSON.parse(body);
+
+            //default values
             var title = '';
             var text = '';
             var author = '';
+
+            //values from reddit api
             if (topPost.data.children.length != 0) {
                 title += topPost.data.children[0].data.title;
                 text += topPost.data.children[0].data.selftext;
                 author += topPost.data.children[0].data.author;
 
             }        
+
+            //send back to frontend script.js
             res.send({
                 postName: title,
                 postText: text, 
